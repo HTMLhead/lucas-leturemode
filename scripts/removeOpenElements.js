77c6b6bf1d146d6414257912c56bdfdf3d2422df
HTMLhead <mrdoo309@gmail.com>
2021-01-12 14:48:15 +0900

    feat(findAndRemoveEle): findAndRemoveEle, removeChildElement함수 제작
    id에 맞는 컴포넌트를 찾아 제거해주는 함수 findAndRemoveEle추가
    id에 OPEN을 판단하여 제거해주도록 추가

--- scripts/removeOpenElements.js
+++ scripts/removeOpenElements.js
@@ -1,14 +1,22 @@
-const stepContainer = document.getElementById("step-list-container");
-const lowerStepContainer = document.getElementById("lowerstep-list-container");
+function findAndRemoveEle() {
+  const stepContainer = document.getElementById("step-list-container");
+  const lowerStepContainer = document.getElementById("lowerstep-list-container");
 
-const removeChildElement = (containerElement) => {
+  if (stepContainer) {
+    removeChildElement(stepContainer);
+  }
+  if (lowerStepContainer) {
+    removeChildElement(lowerStepContainer);
+  }
+}
+
+function removeChildElement(containerElement) {
   const childElements = Array.from(containerElement.children);
   const closedElements = childElements.filter(({ id }) => {
     const splittedIdArr = id.split("-");
-    return splittedIdArr[splittedIdArr.length - 1] === "CLOSED";
+    return splittedIdArr[splittedIdArr.length - 1] !== "OPEN";
   });
   closedElements.forEach((ele) => ele.remove());
-};
+}
 
-removeChildElement(stepContainer);
-removeChildElement(lowerStepContainer);
+findAndRemoveEle();
