da7b0a231d3164788af5c4138b340e4382f8b414
HTMLhead <mrdoo309@gmail.com>
2021-01-13 12:14:05 +0900

    update(createLecturingNotice): 기능 업데이트, 스타일 수정
    이미 강의중 element가 존재하면 아무일도 일어나지 않도록 수정
    bottom의 높이를 2rem으로 수정

--- scripts/createLecturingNotice.js
+++ scripts/createLecturingNotice.js
@@ -1,17 +1,15 @@
 function createLecturingNotice() {
+  if (document.getElementById("lecturing-notice")) return;
   const lecturingNotice = document.createElement("div");
   lecturingNotice.id = "lecturing-notice";
   lecturingNotice.innerHTML = "강의 중";
 
   lecturingNotice.style.fontSize = "2rem";
   lecturingNotice.style.position = "fixed";
-  lecturingNotice.style.bottom = "10rem";
+  lecturingNotice.style.bottom = "2rem";
   lecturingNotice.style.left = "2rem";
 
-  if (!document.getElementById("lecturing-notice")) {
-    console.log("hi");
-    document.getElementById("root").appendChild(lecturingNotice);
-  }
+  document.getElementById("root").appendChild(lecturingNotice);
 }
 
 createLecturingNotice();
